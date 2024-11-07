878a307698994f53c705dadfa872a4bc1224e056
marizqlav <marizqlav@alum.us.es>
2024-11-07 16:47:04 +0100

    fix: Enhanced email sending functionality with dynamic transporter configuration and add password reset tests

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,15 +1,38 @@
 import nodemailer from "nodemailer";
 import { EMAIL_PASS, EMAIL_USER } from "../config";
 
+const getTransporterConfig = (email: string) => {
+  const domain = email.split('@')[1];
+  switch (domain) {
+    case 'gmail.com':
+      return {
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: EMAIL_USER,
+          pass: EMAIL_PASS,
+        },
+      };
+    case 'hotmail.com':
+    case 'outlook.com':
+      return {
+        host: 'smtp-mail.outlook.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: EMAIL_USER,
+          pass: EMAIL_PASS,
+        },
+      };
+    default:
+      throw new Error('Unsupported email domain');
+  }
+};
+
 export const sendEmail = async (to: string, subject: string, text: string) => {
-  console.log(EMAIL_PASS, EMAIL_USER);
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: EMAIL_USER,
-      pass: EMAIL_PASS,
-    },
-  });
+  const transporterConfig = getTransporterConfig(EMAIL_USER);
+  const transporter = nodemailer.createTransport(transporterConfig);
 
   const mailOptions = {
     from: EMAIL_USER,
--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -4,6 +4,9 @@ import app from "../src/app";
 import { API_BASE_URL, AUTH_BASE_URL } from "./utils/constants";
 import { user, badUsers } from "./utils/newUser";
 import { before, after } from "./utils/hooks";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { User } from "../src/models/user";
 
 before;
 after;
@@ -22,6 +25,62 @@ beforeAll(async () => {
 });
 
 describe("User Routes", () => {
+  it("should request a password reset", async () => {
+    const email = user.email;
+    const response = await request(app)
+      .post(`${API_BASE_URL}/user/forgot-password`)
+      .send({ email });
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty(
+      "message",
+      "Correo de restablecimiento de contraseña enviado"
+    );
+
+    const userInDb = await User.findOne({ email });
+    if (!userInDb) {
+      throw new Error("User not found");
+    }
+    const token = jwt.sign({ id: userInDb._id }, "secretKey", {
+      expiresIn: "15m",
+    });
+
+    const newPassword = "NewPassword123!";
+    const resetResponse = await request(app)
+      .post(`${API_BASE_URL}/user/forgot-password/${token}`)
+      .send({ newPassword });
+    expect(resetResponse.status).toBe(200);
+    expect(resetResponse.body).toHaveProperty(
+      "message",
+      "Contraseña actualizada con éxito"
+    );
+
+    const updatedUser = await User.findById(userInDb._id);
+    if (!updatedUser) {
+      throw new Error("Updated user not found");
+    }
+    const isMatch = await bcrypt.compare(newPassword, updatedUser.password);
+    expect(isMatch).toBe(true);
+  });
+
+  it("should not reset password with invalid token", async () => {
+    const invalidToken = "invalidToken";
+    const newPassword = "NewPassword123!";
+    const response = await request(app)
+      .post(`${API_BASE_URL}/user/forgot-password/${invalidToken}`)
+      .send({ newPassword });
+    expect(response.status).toBe(500);
+    expect(response.body).toHaveProperty("message");
+  });
+
+  it("should not request a password reset for a non-existent user", async () => {
+    const email = "nonexistent@example.com";
+    const response = await request(app)
+      .post(`${API_BASE_URL}/user/forgot-password`)
+      .send({ email });
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty("message", "Usuario no encontrado");
+  });
+
   it("should get all users", async () => {
     const response = await request(app)
       .get(`${API_BASE_URL}/users`)
@@ -51,12 +110,12 @@ describe("User Routes", () => {
   });
 
   it("should edit a password of a user by ID", async () => {
-    const currentPassword = user.password;
-    const newUserPassword = "NewPassword123!";
+    const currentPassword = "NewPassword123!";
+    const newUserPassword = "NewPassword12334!";
     const response = await request(app)
       .patch(`${API_BASE_URL}/user/${userId}/password`)
       .set("Authorization", token)
-      .send({ currentPassword, newPassword: newUserPassword });
+      .send({ currentPassword: currentPassword, newPassword: newUserPassword });
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty(
       "message",
